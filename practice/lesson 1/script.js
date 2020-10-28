42bdb1549c6ed917a21a4be2fac9eeb8fe3d256c
KyryloSafonov <kirilsafon@gmail.com>
2020-10-28 20:54:26 +0200

    Practice_1_9

---
+++ practice/lesson 1/script.js
@@ -0,0 +1,81 @@
+/*//Task 1
+let str = "Hello";
+let num = 123;
+let flag = true;
+let txt = "true";
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);*/
+
+/*//Task 2
+let a1 = 5+3;
+let a2 = 5-3;
+let a3 = 5*3;
+let a4 = 5/3;
+let a5 = 5%3;
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+console.log(a5);*/
+
+/*
+//Task3
+let a6 = 5%3;
+let a7 = 3%5;
+let a8 = 5+'3';
+let a9 = '5'-3;
+let a10 = 75 + 'kg';
+console.log(a6);
+console.log(a7);
+console.log(a8);
+console.log(a9);
+console.log(a10);
+*/
+
+/*
+//Task 4
+let height = '23 cm';
+let width = '10 cm';
+let s = parseInt(height)*parseInt(width);
+console.log(s + 'cm^2');
+*/
+
+/*//Task 5
+let heightC = "10m";
+let dC = "4m";
+let v = parseInt(heightC)*parseInt(dC);
+console.log(v + 'cm^3');*/
+
+/*//Task 6
+let n = 3;
+let m = 4;
+let k = Math.pow(n,2) + Math.pow(m,2);
+k = Math.sqrt(k);
+console.log(k);*/
+
+/*
+//Task 7
+let str = 'Hello world'
+console.log(str);
+document.write(str);
+alert(str);
+*/
+
+/*//Task 8
+let FirstName = 'Kyrylo'
+let LastName = 'Safonov'
+let age = '18 years old'
+let hobbi = 'StandUp comic'
+alert(FirstName + '\n' + LastName + '\n' + age + '\n' + hobbi);*/
+
+/*
+//Task 9
+let str1 = 'Who';
+let str2 = 'are';
+let str3 = 'you?'
+let concatenation = str1 + ' ' + str2 + ' ' + str3;
+document.write(concatenation);
+*/
+
