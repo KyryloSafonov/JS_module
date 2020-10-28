a488e3bc64ea4d2248dcb88740baffbeb2a71f92
KyryloSafonov <kirilsafon@gmail.com>
2020-10-28 17:50:27 +0200

    Initial commit

---
+++ Lesson1/script.js
@@ -0,0 +1,183 @@
+/*//Task 1
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let one = 1;
+let ten = 10;
+let three_nine = -999;
+let OneTwoThree = 123;
+let pi = 3.14;
+let twoDot = 2.7
+let sixteen = 16;
+let True = true;
+let False = false;
+
+//console.log() => вывод в консоль
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(three_nine);
+console.log(OneTwoThree);
+console.log(pi);
+console.log(twoDot);
+console.log(sixteen);
+console.log(True);
+console.log(False);
+
+//alert() => вывод алертом
+alert(hello);
+alert(owu);
+alert(com);
+alert(ua);
+alert(one);
+alert(ten);
+alert(three_nine);
+alert(OneTwoThree);
+alert(pi);
+alert(twoDot);
+alert(sixteen);
+alert(True);
+alert(False);
+
+//document.write() => вывод в документ
+document.write(hello +'_');
+document.write(owu +'_');
+document.write(com +'_');
+document.write(ua +'_');
+document.write(one +'_');
+document.write(ten +'_');
+document.write(three_nine +'_')
+document.write(OneTwoThree + '_');
+document.write(pi + '_');
+document.write(twoDot + '_');
+document.write(sixteen + '_');
+document.write(True + '_');
+document.write(False);*/
+
+/*//Task 2
+hello = 'hello world';
+owu = 'okten web';
+com = 'community'
+ua = 'ukraine';
+one = 1.2;
+ten = 10.85;
+three_nine = -999.2578;
+OneTwoThree = 123.123123;
+pi = 3.14159265359;
+twoDot = 2.7514863;
+sixteen = 16.1616;
+True = false;
+False = true;
+
+//console.log() => вывод в консоль
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(three_nine);
+console.log(OneTwoThree);
+console.log(pi);
+console.log(twoDot);
+console.log(sixteen);
+console.log(True);
+console.log(False);
+
+//alert() => вывод алертом
+alert(hello);
+alert(owu);
+alert(com);
+alert(ua);
+alert(one);
+alert(ten);
+alert(three_nine);
+alert(OneTwoThree);
+alert(pi);
+alert(twoDot);
+alert(sixteen);
+alert(True);
+alert(False);
+
+//document.write() => вывод в документ
+document.write(hello +'_');
+document.write(owu +'_');
+document.write(com +'_');
+document.write(ua +'_');
+document.write(one +'_');
+document.write(ten +'_');
+document.write(three_nine +'_')
+document.write(OneTwoThree + '_');
+document.write(pi + '_');
+document.write(twoDot + '_');
+document.write(sixteen + '_');
+document.write(True + '_');
+document.write(False);*/
+
+/*
+//Task 3
+const number1 = 25;
+const number2 = 15;
+const number3 = 14.25;
+const string1 = 'okten';
+const string2 = 'school';
+const string3 = 'programming';
+
+//alert() => вывод alert
+alert(number1);
+alert(number2);
+alert(number3);
+alert(string1);
+alert(string2);
+alert(string3);
+
+//console.log() => вывод в консоль
+console.log(number1);
+console.log(number2);
+console.log(number3);
+console.log(string1);
+console.log(string2);
+console.log(string3);
+
+//document.write => вывод в документ
+document.write(number1 +'_');
+document.write(number2 +'_');
+document.write(number3 +'_');
+document.write(string1 +'_');
+document.write(string2 +'_');
+document.write(string3 +'_');
+*/
+
+/*
+//Task 4
+let FirstName = prompt();
+let LastName = prompt();
+let Patronymic = prompt();
+
+// console.log
+console.log(FirstName);
+console.log(LastName);
+console.log(Patronymic);
+
+//alert()
+alert(FirstName);
+alert(LastName);
+alert(Patronymic);
+
+//document.write()
+document.write(FirstName + '_');
+document.write(LastName + '_');
+document.write(Patronymic);*/
+
+/*//Task 5
+let FirstName = prompt();
+let LastName = prompt();
+let Patronymic = prompt();
+let person = FirstName + LastName + Patronymic;
+console.log(person);
+alert(person);
+document.write(person);*/
