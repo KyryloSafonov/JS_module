e3407d8cc1a5a93e8d209b1a6f6b50bae2c99346
KyryloSafonov <kirilsafon@gmail.com>
2020-10-31 19:53:03 +0200

    Lesson_2

---
+++ Lesson2/script.js
@@ -0,0 +1,183 @@
+/*// Task 1
+let arr = [5,3,6,7,90];
+console.log(arr);
+
+let arr1 = ['my', 'your', 'our', 'best', 'cool'];
+console.log(arr1);
+
+let arr2 = [true, false , 25.67 ,'cool', 'ok'];
+console.log(arr2);
+
+let arr3 = [];
+arr3 [0] = 15;
+arr3 [1] = 'best';
+arr3 [2] = true;
+arr3 [3] = null;
+arr3 [4] = 'begin';
+console.log(arr3);*/
+
+/*
+// Task 2
+//let i ;
+//for (i=0;i<10;i++) {
+  //  document.write('<div>the best day</div>');
+//}
+
+let i;
+for (i=0;i<10;i++) {
+  document.write(`<div>the best day  ${i}</div>`);
+}*/
+
+/*// Task 3
+//let i = 0;
+//while ( i < 20){
+//    document.write('<h1>Happy Birthday</h1>');
+//    i++;
+//}
+
+let i = 0;
+while ( i < 20){
+    document.write(`<h1>Happy Birthday ${i}</h1>`);
+    i++;
+}*/
+
+/*
+// Task 4
+let arr = [10, 20 ,50 , 44.3 , 15 , 16, 18 , 20 , 85, 69];
+for (const number of arr) {
+    console.log(number);
+}*/
+
+/*
+//Task 5
+let arr = ['use', 'web', 'mistake' ,'Happy ' , 'halloween' , 'day', 'break' ,'built','watter' ,'grey']
+for (const string of arr) {
+    console.log(string);
+}*/
+
+/*
+//Task 6
+let arr = ['victory', 125, true, null , false, 'win', [true,1254,'many'],'okten', 'love'];
+for (const arr1 of arr) {
+    console.log(arr1);
+}
+*/
+
+/*//Task 7
+let arr = ['victory', 125, true, null , false, 'win', [true,1254,'many'],'okten', 'love'];
+for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "boolean"){
+        console.log(arr[i]);
+    }
+}*/
+
+/*
+// Task 8
+let arr = ['victory', 125, true, null , false, 'win', [true,1254,'many'],'okten', 'love'];
+for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+        console.log(arr[i]);
+    }
+}
+*/
+
+/*
+// Task 9
+let arr = ['victory', 125, true, null , false, 'win', [true,1254,'many'],'okten', 'love'];
+for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string") {
+        console.log(arr[i]);
+    }
+}
+*/
+
+/*// Task 10
+let arr= [];
+arr[0]= true;
+arr[1]= false;
+arr[2]= 225;
+arr[3]= { name: 'Vasya', age: 19};
+arr[4]= 'delete';
+arr[5]= null;
+arr[6]= undefined;
+arr[7]= 'challange';
+arr[8]= 55;
+arr[9]= 3.14;
+
+for (let arr1 of arr) {
+    console.log(arr1);
+}*/
+
+/*
+// Task 11
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    document.write(i + ' ')
+
+}
+*/
+
+/*// Task 12
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+    document.write(i + ' ')
+
+}*/
+
+/*// Task 13
+for (let i = 0; i < 200; i+= 2) {
+    console.log(i);
+    document.write(i + ' ')
+
+}*/
+
+/*// Task 14
+for (let i = 0; i < 200; i++) {
+    if (i%2 ===0){
+        console.log(i);
+        document.write(i + ' ')
+    }
+
+}*/
+
+/*
+//Task 15
+for (let i = 0; i < 200; i++) {
+    if (i%2 !== 0){
+        console.log(i);
+        document.write(i + ' ')
+    }
+
+}
+*/
+
+/*
+//Task 16
+for (let m = 0; m < 60; m++) {
+    for (let s = 0; s < 60; s++) {
+        document.write(`minute : ${m}  second : ${s} <br>`);
+
+    }
+
+}
+*/
+
+/*
+//Task 17
+for (let h = 0; h < 3; h++) {
+    for (let m = 0; m < 60; m++) {
+        for (let s = 0; s < 60; s++) {
+            document.write(`hours: ${h}  minute : ${m}  second : ${s} <br>`);
+            if (h==2 && m==20){
+                break;
+            }
+        }
+        if (h==2 && m==20){
+            break;
+        }
+
+    }
+
+
+}
+*/
